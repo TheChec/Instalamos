e7bf51873dce37140a4704e9a1583f60a5b35a85
TheChec <chechoosorio@hotmail.com>
2024-06-10 11:38:41 -0500

    navCel

--- instalamos.js
+++ instalamos.js
@@ -74,26 +74,40 @@ document.addEventListener('DOMContentLoaded', function() {
 //Scroll-behavior, por problemas de compatibilidad
 
 $(document).ready(function(){
-    // Add smooth scrolling to all links
     $("a").on('click', function(event) {
   
-      // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
-        // Prevent default anchor click behavior
         event.preventDefault();
   
-        // Store hash
         var hash = this.hash;
   
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+     
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
   
-          // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         });
-      } // End if
-    });
+    }});
   });
+
+
+// NavCel
+
+var contadorNav = 0;
+
+function openNav(){
+    let nav = document.getElementById('navCel');
+    if(contadorNav == 0){
+        nav.style.display = 'flex'
+        nav.style.animation = 'translateNavOpen .6s ease'
+        contadorNav = 1;
+    }else{
+        nav.style.animation = 'translateNavClose .6s ease'
+
+        setTimeout(() => {
+            nav.style.display = 'none'
+            contadorNav = 0;
+        }, 600)
+    }
+}
